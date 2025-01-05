8f1dfd4574c3edce843ba313e4aa17480923048e
LMoca <mocaluigi@yahoo.com>
2025-01-05 15:17:19 -0500

    Changed "Level Up" to "Upgrade' in the Contact section.

--- src/components/CTASection.tsx
+++ src/components/CTASection.tsx
@@ -16,7 +16,7 @@ export default function CTASection() {
   }, []);
 
   return (
-    <Section id="contact" title="Ready to Level Up Your Business?">
+    <Section id="contact" title="Ready to Upgrade your Business?">
       <div className="max-w-4xl mx-auto text-center">
         <motion.div
           variants={fadeInUpVariant}
